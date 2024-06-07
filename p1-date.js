66b0616e1f963c18f577e95ddb8f35f4677c9a2c
opyle <166080658+opyle@users.noreply.github.com>
2024-06-07 13:12:37 -0700

    Create p1-date.js

---
+++ p1-date.js
@@ -0,0 +1,5 @@
+/*
+    CIT 281 Project 1
+    Name: Owen Pyle
+*/
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; console.log(daysOfWeek[new Date().getDay()]);
